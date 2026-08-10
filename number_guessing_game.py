2dd4775de5954dea5dc8899f8613e52d2e439fab
John Carl V. Gailan <johncarlgailan@gmail.com>
2026-08-10 17:57:18 +0800

    Number guessing game V1

---
+++ number_guessing_game.py
@@ -0,0 +1,35 @@
+import random
+
+print("Welcome the number guessing game! Guess a number from 1 to 100.")
+
+secret_number = random.randint(1, 100)
+attempts = 0
+
+while True: 
+
+    user_guess = int(input("Enter a number: "))
+    attempts += 1
+
+    if user_guess > secret_number:
+         print("Too high")      
+    elif user_guess < secret_number:
+         print("Too low")
+    else:
+         print("Correct!")
+         break
+
+print("Attempts: ", attempts)
+
+
+
+         
+    
+
+    
+
+  
+
+
+
+
+
